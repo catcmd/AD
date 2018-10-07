673223ca60490c054a2a04b07ac5cdc8629eee46
Carolime <ariesu@outlook.de>
2018-10-07 19:41:09 +0200

    Successfully applied bubble sort. :)

---
+++ uebung1/src/main/java/BubbleSort.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] a = {35, 22, 10, 51, 48};
+        System.out.println("Eingabe: " + Arrays.toString(a));
+        bubbleSort(a);
+        System.out.println("Ausgabe: " + Arrays.toString(a));
+    }
+
+    public static void bubbleSort (int[] a)
+    {
+        int tmp = 0;
+        for(int i = 0; i < a.length - 1; i++)
+        {
+            for(int j = a.length - 1; j > i; j--)
+            {
+                if(a[j] < a[j - 1])
+                {
+                  tmp = a[j];
+                  a[j] =  a[j - 1];
+                  a[j - 1] = tmp;
+                }
+            }
+        }
+    }
+}
